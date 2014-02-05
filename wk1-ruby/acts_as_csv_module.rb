b8b66b498844d3596a4b1592de968cd7b52b7bd0
ciaranj <ciaranj@gmail.com>
2014-02-05 21:04:53 +0000

    Day 3 (last day, sniff :( )

--- wk1-ruby/acts_as_csv_module.rb
+++ wk1-ruby/acts_as_csv_module.rb
@@ -7,7 +7,19 @@ module ActsAsCSV
       include InstanceMethods
     end
   end
-  
+
+  class CsvRow
+    attr_accessor :headers, :row
+    def initialize(headers, row)
+      @headers= headers
+      @row= row
+    end
+    def method_missing(name, *args)
+      i= @headers.index(name.to_s)
+      @row[i] # Meh out of range pain here, meh for now ;)
+    end
+  end
+
   module InstanceMethods
     def read
       @csv_contents =[]
@@ -18,9 +30,15 @@ module ActsAsCSV
         @csv_contents << row.chomp.split(', ')
       end
     end
-    
+
+    def each &rowreader
+      @csv_contents.each do |row|
+        yield CsvRow.new( @headers, row )
+      end
+    end
+
     attr_accessor :headers, :csv_contents
-    
+
     def initialize
       read
     end
@@ -32,6 +50,5 @@ class RubyCsv
   acts_as_csv
 end
 
-m= RubyCsv.new
-puts m.headers.inspect
-puts m.csv_contents.inspect
+csv= RubyCsv.new
+csv.each {|row| puts row.one}
